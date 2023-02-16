fbe54cb89f1961035cbe9f25407ec7a781b4b5d1
checkwoodson <1194542373@qq.com>
2023-02-16 18:14:14 +0800

    feat:新增scheduler

--- src/reactivity/__tests__/effect.spec.ts
+++ src/reactivity/__tests__/effect.spec.ts
@@ -18,7 +18,7 @@ describe('effect', function () {
 
         // * set -> 触发依赖
         user.age++;
-        // expect(nextAge).toBe(12);
+        expect(nextAge).toBe(12);
     });
     it('runner', () => {
         let foo = 10;
@@ -31,4 +31,29 @@ describe('effect', function () {
         expect(foo).toBe(12); //第二次执行 foo++
         expect(r).toBe('foo');
     })
+    it('scheduler', () => {
+        let dummy;
+        let run: any;
+        const scheduler = jest.fn(() => {
+            run = runner;
+        });
+        const obj = reactive({ foo: 1 });
+        const runner = effect(
+            () => {
+                dummy = obj.foo;
+            },
+            { scheduler },
+        );
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1);
+        // should be called on first trigger
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        // should not run yet
+        expect(dummy).toBe(1);
+        // manually run
+        run();
+        // should have run
+        expect(dummy).toBe(2);
+    });
 });
--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -2,7 +2,7 @@
 let activeEffect
 class ReactiveEffect {
     private _fn
-    constructor(fn) {
+    constructor(fn, public scheduler?) {
         this._fn = fn
     }
     run() {
@@ -11,8 +11,8 @@ class ReactiveEffect {
     }
 }
 
-export function effect(fn) {
-    const _effect = new ReactiveEffect(fn)
+export function effect(fn, options: any = {}) {
+    const _effect = new ReactiveEffect(fn, options.scheduler)
     _effect.run()
     // 当前调用的_effect指向ReactiveEffect实例
     return _effect.run.bind(_effect)
@@ -34,7 +34,11 @@ export function track(target, key) {
 export function trigger(target, key) {
     let depsMap = targetMap.get(target) //获取目标对象
     let dep = depsMap.get(key) //取值
-    // for (let effect of dep) {
-    //     effect.run()
-    // }
+    for (let effect of dep) {
+        if (effect.scheduler) {
+            effect.scheduler()
+        } else {
+            effect.run()
+        }
+    }
 }
