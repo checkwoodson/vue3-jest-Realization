6ef0f62fc23aeb1093c3400c4adbe1f1c63e2e3f
checkwoodson <1194542373@qq.com>
2023-02-21 18:33:12 +0800

    feat：抽离公共模块

--- src/reactivity/reactivity.ts
+++ src/reactivity/reactivity.ts
@@ -1,35 +1,30 @@
 import { track, trigger } from './effect'
 export function reactive(raw: Object) {
   return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key);
-      track(target, key)
-      return res;
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
-      trigger(target, key)
-      return res;
-    },
+    get: createGetter(),
+    set: createSetter()
   });
 }
 
 export function readonly(target) {
   return new Proxy(target, {
-    get(target, key) {
-      return Reflect.get(target, key);
-    },
-    set(target,key,value) {
-      console.warn('只能读取，不能修改')
-      return true
-    }
+    get: createGetter(true),
+    set: createSetter(true)
   })
 }
 
-function createGetter(isReadonly = false){
-  return function get(target,key){
-    const res = Reflect.get(target,key)
-    !isReadonly && track(target,key)
+function createGetter(isReadonly = false) {
+  return (target, key) => {
+    const res = Reflect.get(target, key)
+    !isReadonly && track(target, key)
     return res
+  }
+}
+function createSetter(isReadonly = false) {
+  return (target, key, value) => {
+    if(isReadonly) return true;
+    const res = Reflect.set(target, key, value);
+    trigger(target, key)
+    return res;
   }
 }
