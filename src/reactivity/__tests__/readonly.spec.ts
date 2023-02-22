4a1b00bb7f9839c2905c633b17189fe98a61176b
checkwoodson <1194542373@qq.com>
2023-02-22 18:01:23 +0800

    feat🛠:完善readonly的set报错情况

--- src/reactivity/__tests__/readonly.spec.ts
+++ src/reactivity/__tests__/readonly.spec.ts
@@ -10,5 +10,10 @@ describe('readonly', () => {
         expect(wrapped.foo).toBe(1)
 
     })
-
+    it('should call console.warn when set', () =>{
+        console.warn = jest.fn()
+        const user = readonly({ age :10})
+        user.age =11;
+        expect(console.warn).toBeCalled()
+    })
 })
--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,6 +1,6 @@
 import { track, trigger } from './effect'
 class ProxyInterface {
-  createGetter(isReadonly = false ) {
+  createGetter(isReadonly = false) {
     return (target, key) => {
       const res = Reflect.get(target, key)
       !isReadonly && track(target, key)
@@ -9,7 +9,10 @@ class ProxyInterface {
   }
   createSetter(isReadonly = false) {
     return (target, key, value) => {
-      if (isReadonly) return true;
+      if (isReadonly) {
+        console.warn(`Set operation on key "${String(key)}" failed: target is readonly.`, target)
+        return true
+      }
       const res = Reflect.set(target, key, value);
       trigger(target, key)
       return res;
@@ -17,16 +20,16 @@ class ProxyInterface {
   }
 }
 
-export default function reactiveHandler () {
-    const { createGetter, createSetter } = new ProxyInterface()
-    return {
-        reactiveBase:{
-            get: createGetter(),
-            set: createSetter()
-        },
-        isReadonly:{
-            get: createGetter(true),
-            set: createSetter(true)
-        }
+export default function reactiveHandler() {
+  const { createGetter, createSetter } = new ProxyInterface()
+  return {
+    reactiveBase: {
+      get: createGetter(),
+      set: createSetter()
+    },
+    isReadonly: {
+      get: createGetter(true),
+      set: createSetter(true)
     }
+  }
 }
