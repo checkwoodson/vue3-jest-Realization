1eff91023a59313ae688eaf3b65bd9aa4b4a277b
checkwoodson <1194542373@qq.com>
2023-02-22 14:22:42 +0800

    将代理的get和set改成class写，不过好像感觉还是函数式更好一些。以后再看看吧

--- src/reactivity/reactivity.ts
+++ src/reactivity/reactivity.ts
@@ -1,4 +1,23 @@
 import { track, trigger } from './effect'
+class Rproxy {
+  createGetter(isReadonly = false ) {
+    return (target, key) => {
+      const res = Reflect.get(target, key)
+      !isReadonly && track(target, key)
+      return res
+    }
+  }
+  createSetter(isReadonly = false) {
+    return (target, key, value) => {
+      if (isReadonly) return true;
+      const res = Reflect.set(target, key, value);
+      trigger(target, key)
+      return res;
+    }
+  }
+}
+
+const { createGetter, createSetter } = new Rproxy()
 export function reactive(raw: Object) {
   return new Proxy(raw, {
     get: createGetter(),
@@ -13,18 +32,3 @@ export function readonly(target) {
   })
 }
 
-function createGetter(isReadonly = false) {
-  return (target, key) => {
-    const res = Reflect.get(target, key)
-    !isReadonly && track(target, key)
-    return res
-  }
-}
-function createSetter(isReadonly = false) {
-  return (target, key, value) => {
-    if(isReadonly) return true;
-    const res = Reflect.set(target, key, value);
-    trigger(target, key)
-    return res;
-  }
-}
